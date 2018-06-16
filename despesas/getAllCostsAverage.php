802ec1139981e4a0db479879a86d234864a799bb
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2018-06-16 20:34:50 -0300

    Primeira API

---
+++ despesas/getAllCostsAverage.php
@@ -0,0 +1,62 @@
+<?php
+
+
+    require_once("../../../functions.php");
+    $database = connect_DB();
+    
+    $mes_inicial = @$_GET["mesIni"];
+    $ano_inicial = @$_GET["anoIni"];
+
+    $mes_final = @$_GET["mesFim"];
+    $ano_final = @$_GET["anoFim"];
+
+    $linhas = array();
+    
+    $where_clause = "";
+
+    //CASO TENHA RANGE FINAL E RANGE INICIAL
+    if(!empty($ano_inicial) && !empty($mes_inicial) && !empty($ano_final) && !empty($mes_final))
+    {
+        $where_clause = "WHERE ano >= $ano_inicial AND ano <= $ano_final AND mes >= $mes_inicial AND mes <= $mes_final";
+    }
+    else if(!empty($ano_inicial) && !empty($mes_inicial))
+    {
+        $where_clause = "WHERE ano >= $ano_inicial AND mes >= $mes_inicial ";
+    }
+    else if(!empty($ano_final) && !empty($mes_final))
+    {
+        $where_clause = "WHERE ano <= $ano_inicial AND mes <= $mes_inicial ";
+    }
+
+    $sql = "SELECT SUM(valor) AS total,
+                        despesa,
+                        mes,
+                        ano,
+                        COUNT(*) AS qtd,
+                        SUM(valor) / count(*) AS media 
+                        FROM `debitovereador` $where_clause GROUP BY despesa";
+
+    $result = @mysqli_query($database,$sql);
+
+    while($row = @mysqli_fetch_assoc($result))
+    {
+        $total = $row["total"];
+        $despesa = $row["despesa"];
+        $qtd = $row["qtd"];
+        $media = $row["media"];
+        //$mes = $row["mes"];
+        //$ano = $row["ano"];
+
+        $linha = array('total'=>$total,
+                        'despesa'=>$despesa,
+                        'qtd'=>$qtd,
+                        'media'=>$media);
+
+        array_push($linhas, $linha);
+
+        //echo $total."<br>";
+    }
+    
+    echo json_encode($linhas);
+
+?>
---
+++ despesas/getAllCostsMonths.php
@@ -0,0 +1,26 @@
+<?php
+
+    require_once("../../../functions.php");
+    $database = connect_DB();
+    
+    $linhas = array();
+
+    $sql = "SELECT SUM(valor) AS total, mes FROM `debitovereador` GROUP BY mes";
+    $result = @mysqli_query($database,$sql);
+
+    while($row = @mysqli_fetch_assoc($result))
+    {
+        $total = $row["total"];
+        $mes = $row["mes"];
+
+        $linha = array('total'=>$total,
+                        'mes'=>$mes);
+
+        array_push($linhas, $linha);
+
+        //echo $total."<br>";
+    }
+    
+    echo json_encode($linhas);
+
+?>
